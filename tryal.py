4b42886c1d99ec769ee7de20010c6fd8b2817d1f
moundu <munenemuondu@gmail.com>
2020-08-18 11:51:35 +0300

    I have made some changes

--- tryal.py
+++
@@ -1,7 +0,0 @@
-def triangle(base,height):
-    area = base *  height / 2
-    return area
-    # if area < 60:
-    #     return base - height
-a = triangle(6,5,4++)
-print(a)
--- tryal4.py
+++
@@ -1,4 +0,0 @@
-def triangle(base=1,height=4):
-    area = base * height / 2
-    return area
-print(triangle())
